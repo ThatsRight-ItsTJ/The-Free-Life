2eb2a477ea8c862dba094b025ccc338663f29062
TJ Heid <169084002+ThatsRight-ItsTJ@users.noreply.github.com>
2025-03-07 10:10:10 -0600

    Update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -1,12 +1,12 @@
 export const GITHUB_REPO = {
-  owner: 'Offren',  // replace this with your Github Username
-  name: 'gitWiki', // replace this with your repo's name
+  owner: 'ThatsRight-ItsTJ',  // replace this with your Github Username
+  name: 'The-Free-Life', // replace this with your repo's name
   branch: 'main',
   contentPath: 'content'
 }
 
 // Configure your discussion/talk URL here
-export const TALK_URL = 'https://github.com/Offren/gitWiki/discussions' // replace this with whatever site you want
+export const TALK_URL = 'https://github.com/ThatsRight-ItsTJ/The-Free-Life/discussions' // replace this with whatever site you want
 
 export const getGithubEditUrl = (path: string): string => {
   return `https://github.com/${GITHUB_REPO.owner}/${GITHUB_REPO.name}/edit/${GITHUB_REPO.branch}/${GITHUB_REPO.contentPath}/${path}`
